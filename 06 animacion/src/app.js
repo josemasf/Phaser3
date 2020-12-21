30884c7d76868f08132429866694307961d4d66d
josemasf <josemasf@gmail.com>
2020-12-21 11:28:52 +0100

    cargo lógica desde JSON

--- 06 animacion/src/app.js
+++ 06 animacion/src/app.js
@@ -13,26 +13,23 @@ const config = {
 new Phaser.Game(config);
 
 function preload(){
-    console.log('preload')
+    console.log('preload');
+
+    this.load.atlas('tomato_atlas', './assets/evil_tomato.png', './assets/evil_tomato.json');
+    this.load.json('evil_tomato_anim', './assets/evil_tomato_anim.json');
 
     this.load.spritesheet('evil_tomato', './assets/evil_tomato.png', {frameWidth: 16, frameHeight: 25})
 }
 function create(){
     console.log('create')
 
-    this.tomato = this.add.sprite(100,100,'evil_tomato').setScale(2)
+    this.tomato = this.add.sprite(100,100,'evil_tomato').setScale(2);
+    this.dataAnim = this.cache.json.get('evil_tomato_anim')
 
-    this.anims.create({
-        key: 'tomato_walk',
-        frames: this.anims.generateFrameNumbers('evil_tomato',{
-            start: 1,
-            end: 8
-        }),
-        repeat: -1,
-        frameRate: 10
-    });
+    this.anims.fromJSON(this.dataAnim);
 
-    this.tomato.anims.play('tomato_walk');
+    this.tomato.anims.play('tomato_atlas_walk');
+    //this.tomato.anims.play('tomato_atlas_idle');
 
     
 }
